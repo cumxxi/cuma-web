9fb62ef2f9ec9c1acbc51e92b364c40f0f20e104
cumxxi <cuma@gmail.com>
2018-12-07 21:48:54 +0300

    Inital commit

---
+++ Web/Models/Index.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+
+namespace Web.Models
+{
+    public class Index : PageModel
+    {
+    }
+}
---
+++ Web/Models/Products/Index.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+
+namespace Web.Models.Products
+{
+    public class Index : PageModel
+    {
+    }
+}
